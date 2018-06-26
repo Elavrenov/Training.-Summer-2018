bfe2ccd804090b114157c64ff25d7b17064e4b4b
Egor Lavrenov <egorlavenow@gmail.com>
2018-06-26 23:41:39 +0300

    Task

---
+++ NET.S.2018.Lavrenov.01/IntegerQuickSort.Tests/IntQuickSort.Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntegerQuickSort.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IntQuickSort_ArrayWasSorted()
+        {
+            int[] someArray = new[] {3,2,1,0,-5,4,4,5};
+            int[] expected = new[] {-5,0,1,2,3,4,4,5};
+
+            IntQuickSort.QuickSort(someArray);   
+
+            CollectionAssert.AreEqual(expected,someArray);
+        }
+
+        [TestMethod, ExpectedException(typeof(NullReferenceException))]
+        public void IntQuickSort_ArrayWithNullReference_ThrowNullReferenceException()
+        {
+            int[] someArray = null;
+            IntQuickSort.IsValidArray(someArray);
+            
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void IntQuickSort_ArrayWithNullArgumenr_ThrowNoNullAllowedException()
+        {
+            int[] someArray = new int[0];
+            IntQuickSort.IsValidArray(someArray);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void IntQuickSort_ArrayWithNoSize_ThrowArgumentException()
+        {
+            int[] someArray =new int[1];
+            IntQuickSort.IsValidArray(someArray);
+        }
+
+    }
+}
---
+++ NET.S.2018.Lavrenov.01/IntegerQuickSort.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IntegerQuickSort.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IntegerQuickSort.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("193f5eca-4217-4d1d-9ad3-06475fa6aca0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
