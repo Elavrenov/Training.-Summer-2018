18eba93ebbf227da11c7d8516d0ec0d081c52df4
Egor Lavrenov <egorlavenow@gmail.com>
2018-07-19 21:50:02 +0300

    Edits with delegates

--- NET.S.2018.Lavrenov.03/Task2/GCDFinder.cs
+++ NET.S.2018.Lavrenov.03/Task2/GCDFinder.cs
@@ -8,6 +8,8 @@ namespace Task2
     /// </summary>
     public static class GcdFinder
     {
+        #region Public API
+
         #region Public API EuclideanGCD
 
         /// <summary>
@@ -19,21 +21,7 @@ namespace Task2
         /// <returns>GCD of 2 elements</returns>
         public static int EuclideanAlgorithm(int firstNumber, int secondNumber)
         {
-            firstNumber = Math.Abs(firstNumber);
-            secondNumber = Math.Abs(secondNumber);
-
-            while (firstNumber != secondNumber)
-            {
-                if (firstNumber > secondNumber)
-                {
-                    firstNumber = firstNumber - secondNumber;
-                }
-                else
-                {
-                    secondNumber = secondNumber - firstNumber;
-                }
-            }
-            return firstNumber;
+            return GetEuclidianGcd(EuclidGcd, firstNumber, secondNumber);
         }
 
         /// <summary>
@@ -49,7 +37,7 @@ namespace Task2
             Stopwatch timeWorking = new Stopwatch();
             timeWorking.Start();
 
-            int gcd = EuclideanAlgorithm(first, second);
+            int gcd = GetEuclidianGcd(EuclidGcd, first, second);
 
             timeWorking.Stop();
 
@@ -67,7 +55,7 @@ namespace Task2
         /// <returns>GCD of 3 elements</returns>
         public static int EuclideanAlgorithm(int first, int second, int third)
         {
-            return EuclideanAlgorithm(EuclideanAlgorithm(first, second), third);
+            return GetEuclidianGcd(EuclidGcd, first, second, third);
         }
 
         /// <summary>
@@ -84,7 +72,7 @@ namespace Task2
             Stopwatch timeWorking = new Stopwatch();
             timeWorking.Start();
 
-            int gcd = EuclideanAlgorithm(first, second, third);
+            int gcd = GetEuclidianGcd(EuclidGcd, first, second, third);
 
             timeWorking.Stop();
 
@@ -99,19 +87,7 @@ namespace Task2
         /// <returns> GCD of all numbers </returns>
         public static int EuclideanAlgorithm(params int[] numbers)
         {
-            if (numbers.Length == 0 || numbers.Length == 1)
-            {
-                throw new ArgumentException($"{nameof(numbers)} must be greater than 1");
-            }
-
-            int answer = numbers[0];
-
-            for (int i = 0; i < numbers.Length - 1; i++)
-            {
-                answer = EuclideanAlgorithm(answer, numbers[++i]);
-            }
-
-            return answer;
+            return GetEuclidianGcd(EuclidGcd, numbers);
         }
 
         /// <summary>
@@ -125,7 +101,7 @@ namespace Task2
             Stopwatch timeWorking = new Stopwatch();
             timeWorking.Start();
 
-            int gcd = EuclideanAlgorithm(numbers);
+            int gcd = GetEuclidianGcd(EuclidGcd, numbers);
 
             timeWorking.Stop();
 
@@ -145,6 +121,205 @@ namespace Task2
         /// <param name="secondNumber">second number</param>
         /// <returns>GCD of 2 elements</returns>
         public static int BinaryEuclideanAlgorithm(int firstNumber, int secondNumber)
+        {
+            return GetSteinGcd(SteinGcd, firstNumber, secondNumber);
+        }
+        /// <summary>
+        /// The method for searching GCD(Binary Euclidian algorithm)
+        /// 2 arguments
+        /// </summary>
+        /// <param name="elapsedTimeMiliSecs">algorithm working time</param>
+        /// <param name="first">first element</param>
+        /// <param name="second">second element</param>
+        /// <returns>GCD of 2 elements and elapsed time in milliseconds</returns>
+        public static int BinaryEuclideanAlgorithm(out long elapsedTimeMiliSecs, int first, int second)
+        {
+            Stopwatch timeWorking = new Stopwatch();
+            timeWorking.Start();
+
+            int gcd = GetSteinGcd(SteinGcd, first, second);
+
+            timeWorking.Stop();
+
+            elapsedTimeMiliSecs = timeWorking.Elapsed.Milliseconds;
+            return gcd;
+        }
+
+        /// <summary>
+        /// The method for searching GCD(Binary Euclidian algorithm)
+        /// 3 arguments
+        /// </summary>
+        /// <param name="first">first number</param>
+        /// <param name="second">second number</param>
+        /// <param name="third">third number</param>
+        /// <returns>GCD of 3 elements</returns>
+        public static int BinaryEuclideanAlgorithm(int first, int second, int third)
+        {
+            return GetSteinGcd(SteinGcd, first, second, third);
+        }
+
+        /// <summary>
+        /// The method for searching GCD(Binary Euclidian algorithm)
+        /// 3 arguments
+        /// </summary>
+        /// <param name="elapsedTimeMiliSecs">algorithm working time</param>
+        /// <param name="first">first element</param>
+        /// <param name="second">second element</param>
+        /// <param name="third">third element</param>
+        /// <returns>GCD of 3 elements and elapsed time in milliseconds</returns>
+        public static int BinaryEuclideanAlgorithm(out long elapsedTimeMiliSecs, int first, int second, int third)
+        {
+            Stopwatch timeWorking = new Stopwatch();
+            timeWorking.Start();
+
+            int gcd = GetSteinGcd(SteinGcd, first, second, third);
+
+            timeWorking.Stop();
+
+            elapsedTimeMiliSecs = timeWorking.Elapsed.Milliseconds;
+            return gcd;
+        }
+
+        /// <summary>
+        /// The method for searching GCD(Binary Euclidian algorithm)
+        /// </summary>
+        /// <param name="numbers">integers</param>
+        /// <returns>GCD of all numbers</returns>
+        public static int BinaryEuclideanAlgorithm(params int[] numbers)
+        {
+            return GetSteinGcd(SteinGcd, numbers);
+        }
+
+        /// <summary>
+        /// The method for searching GCD(Binary Euclidian algorithm)
+        /// </summary>
+        /// <param name="elapsedTimeMiliSecs">algorithm working time</param>
+        /// <param name="numbers">integers</param>
+        /// <returns>GCD of all numbers and elapsed time in milliseconds</returns>
+        public static int BinaryEuclideanAlgorithm(out long elapsedTimeMiliSecs, params int[] numbers)
+        {
+            Stopwatch timeWorking = new Stopwatch();
+            timeWorking.Start();
+
+            int gcd = GetSteinGcd(SteinGcd, numbers);
+
+            timeWorking.Stop();
+
+            elapsedTimeMiliSecs = timeWorking.Elapsed.Milliseconds;
+            return gcd;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region PrivateMethods
+
+        #region Private Euclidian methods
+
+        private static int GetEuclidianGcd(Func<int, int, int> gcdFunc, int firstNumber, int secondNumber)
+        {
+            if (gcdFunc == null) throw new ArgumentNullException($"{nameof(gcdFunc)} can't be null");
+            if (firstNumber == 0 && secondNumber == 0)
+            {
+                throw new ArgumentException($"Wrong parameters");
+            }
+
+            return gcdFunc(firstNumber, secondNumber);
+
+        }
+
+        private static int GetEuclidianGcd(Func<int, int, int> gcdFunc, int firstNumber, int secondNumber, int thirdNumber)
+        {
+            if (gcdFunc == null) throw new ArgumentNullException($"{nameof(gcdFunc)} can't be null");
+            if (firstNumber == 0 && secondNumber == 0 && thirdNumber == 0)
+            {
+                throw new ArgumentException($"Wrong parameters");
+            }
+
+            return gcdFunc(gcdFunc(firstNumber, secondNumber), thirdNumber);
+        }
+
+        private static int GetEuclidianGcd(Func<int, int, int> gcdFunc, params int[] numbers)
+        {
+            if (gcdFunc == null) throw new ArgumentNullException($"{nameof(gcdFunc)} can't be null");
+
+
+            if (numbers.Length == 0 || numbers.Length == 1)
+            {
+                throw new ArgumentException($"{nameof(numbers)} must be greater than 1");
+            }
+
+            int answer = numbers[0];
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                answer = gcdFunc(answer, numbers[++i]);
+            }
+
+            return answer;
+        }
+        private static int EuclidGcd(int firstNumber, int secondNumber)
+        {
+            firstNumber = Math.Abs(firstNumber);
+            secondNumber = Math.Abs(secondNumber);
+
+            while (firstNumber != secondNumber)
+            {
+                if (firstNumber > secondNumber)
+                {
+                    firstNumber = firstNumber - secondNumber;
+                }
+                else
+                {
+                    secondNumber = secondNumber - firstNumber;
+                }
+            }
+            return firstNumber;
+        }
+
+        #endregion
+
+        #region Private Stein methods
+
+        private static int GetSteinGcd(Func<int, int, int> gcdFunc, params int[] numbers)
+        {
+            if (numbers.Length == 0 || numbers.Length == 1)
+            {
+                throw new ArgumentException($"{nameof(numbers)} must be greater than 1");
+            }
+
+            int answer = numbers[0];
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                answer = gcdFunc(answer, numbers[++i]);
+            }
+
+            return answer;
+        }
+
+        private static int GetSteinGcd(Func<int, int, int> gcdFunc, int firstNumber, int secondNumber, int thirdNumber)
+        {
+            if (gcdFunc == null) throw new ArgumentNullException($"{nameof(gcdFunc)} can't be null");
+            if (firstNumber == 0 && secondNumber == 0 && thirdNumber == 0)
+            {
+                throw new ArgumentException($"Wrong parameters");
+            }
+
+            return gcdFunc(gcdFunc(firstNumber, secondNumber), thirdNumber);
+        }
+
+        private static int GetSteinGcd(Func<int, int, int> gcdFunc, int firstNumber, int secondNumber)
+        {
+            if (gcdFunc == null) throw new ArgumentNullException($"{nameof(gcdFunc)} can't be null");
+            if (firstNumber == 0 && secondNumber == 0)
+            {
+                throw new ArgumentException($"Wrong parameters");
+            }
+
+            return gcdFunc(firstNumber, secondNumber);
+        }
+        private static int SteinGcd(int firstNumber, int secondNumber)
         {
             firstNumber = Math.Abs(firstNumber);
             secondNumber = Math.Abs(secondNumber);
@@ -168,120 +343,26 @@ namespace Task2
             {
                 if ((secondNumber & 1) != 0)
                 {
-                    return BinaryEuclideanAlgorithm(firstNumber >> 1, secondNumber);
+                    return SteinGcd(firstNumber >> 1, secondNumber);
                 }
 
-                return BinaryEuclideanAlgorithm(firstNumber >> 1, secondNumber >> 1) << 1;
+                return SteinGcd(firstNumber >> 1, secondNumber >> 1) << 1;
             }
 
             if ((~secondNumber & 1) != 0)
             {
-                return BinaryEuclideanAlgorithm(firstNumber, secondNumber >> 1);
+                return SteinGcd(firstNumber, secondNumber >> 1);
             }
 
             if (firstNumber > secondNumber)
             {
-                return BinaryEuclideanAlgorithm((firstNumber - secondNumber) >> 1, secondNumber);
+                return SteinGcd((firstNumber - secondNumber) >> 1, secondNumber);
             }
 
-            return BinaryEuclideanAlgorithm((secondNumber - firstNumber) >> 1, firstNumber);
-        }
-        /// <summary>
-        /// The method for searching GCD(Binary Euclidian algorithm)
-        /// 2 arguments
-        /// </summary>
-        /// <param name="elapsedTimeMiliSecs">algorithm working time</param>
-        /// <param name="first">first element</param>
-        /// <param name="second">second element</param>
-        /// <returns>GCD of 2 elements and elapsed time in milliseconds</returns>
-        public static int BinaryEuclideanAlgorithm(out long elapsedTimeMiliSecs, int first, int second)
-        {
-            Stopwatch timeWorking = new Stopwatch();
-            timeWorking.Start();
-
-            int gcd = BinaryEuclideanAlgorithm(first, second);
-
-            timeWorking.Stop();
-
-            elapsedTimeMiliSecs = timeWorking.Elapsed.Milliseconds;
-            return gcd;
+            return SteinGcd((secondNumber - firstNumber) >> 1, firstNumber);
         }
 
-        /// <summary>
-        /// The method for searching GCD(Binary Euclidian algorithm)
-        /// 3 arguments
-        /// </summary>
-        /// <param name="first">first number</param>
-        /// <param name="second">second number</param>
-        /// <param name="third">third number</param>
-        /// <returns>GCD of 3 elements</returns>
-        public static int BinaryEuclideanAlgorithm(int first, int second, int third)
-        {
-            return BinaryEuclideanAlgorithm(BinaryEuclideanAlgorithm(first, second), third);
-        }
-
-        /// <summary>
-        /// The method for searching GCD(Binary Euclidian algorithm)
-        /// 3 arguments
-        /// </summary>
-        /// <param name="elapsedTimeMiliSecs">algorithm working time</param>
-        /// <param name="first">first element</param>
-        /// <param name="second">second element</param>
-        /// <param name="third">third element</param>
-        /// <returns>GCD of 3 elements and elapsed time in milliseconds</returns>
-        public static int BinaryEuclideanAlgorithm(out long elapsedTimeMiliSecs, int first, int second, int third)
-        {
-            Stopwatch timeWorking = new Stopwatch();
-            timeWorking.Start();
-
-            int gcd = BinaryEuclideanAlgorithm(first, second, third);
-
-            timeWorking.Stop();
-
-            elapsedTimeMiliSecs = timeWorking.Elapsed.Milliseconds;
-            return gcd;
-        }
-
-        /// <summary>
-        /// The method for searching GCD(Binary Euclidian algorithm)
-        /// </summary>
-        /// <param name="numbers">integers</param>
-        /// <returns>GCD of all numbers</returns>
-        public static int BinaryEuclideanAlgorithm(params int[] numbers)
-        {
-            if (numbers.Length == 0 || numbers.Length == 1)
-            {
-                throw new ArgumentException($"{nameof(numbers)} must be greater than 1");
-            }
-
-            int answer = numbers[0];
-
-            for (int i = 0; i < numbers.Length - 1; i++)
-            {
-                answer = BinaryEuclideanAlgorithm(answer, numbers[++i]);
-            }
-
-            return answer;
-        }
-
-        /// <summary>
-        /// The method for searching GCD(Binary Euclidian algorithm)
-        /// </summary>
-        /// <param name="elapsedTimeMiliSecs">algorithm working time</param>
-        /// <param name="numbers">integers</param>
-        /// <returns>GCD of all numbers and elapsed time in milliseconds</returns>
-        public static int BinaryEuclideanAlgorithm(out long elapsedTimeMiliSecs, params int[] numbers)
-        {
-            Stopwatch timeWorking = new Stopwatch();
-            timeWorking.Start();
-
-            int gcd = BinaryEuclideanAlgorithm(numbers);
-
-            timeWorking.Stop();
-
-            elapsedTimeMiliSecs = timeWorking.Elapsed.Milliseconds;
-            return gcd;
-        }
+        #endregion
 
         #endregion
     }
