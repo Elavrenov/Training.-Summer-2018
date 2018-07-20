536fd943b9deb03d8e7cf7f851962e76375df8b1
Egor Lavrenov <egorlavenow@gmail.com>
2018-07-20 18:09:19 +0300

    Interface & Delegate methods for sorting jagged adrray + Tests

--- NET.S.2018.Lavrenov.09/JaggedArraySorts/DelegateMethodJaggedArrayBubbleSort.cs
+++ NET.S.2018.Lavrenov.09/JaggedArraySorts/DelegateMethodJaggedArrayBubbleSort.cs
@@ -26,22 +26,29 @@
                 throw new ArgumentNullException($"{nameof(conditionFunc)} can't be null");
             }
 
-            BubbleSort(array, new Decorator(conditionFunc));
+            BubbleSort(array, new Adapter(conditionFunc));
         }
 
-        #endregion
-
-        #region Private methods
-
-        private static void Swap(ref int[] firstRow, ref int[] secondRow)
+        /// <summary>
+        /// Method for sorting jagged arrays
+        /// with given comparision logic
+        /// </summary>
+        /// <param name="array">Array for sorting</param>
+        /// <param name="comparer">Necessary logic for sorting</param>
+        /// <exception cref="ArgumentNullException">if given array is null</exception>
+        /// <exception cref="ArgumentNullException">if given comparer is null</exception>
+        public static void BubbleSort(int[][] array, IComparer<int[]> comparer)
         {
-            int[] temp = firstRow;
-            firstRow = secondRow;
-            secondRow = temp;
-        }
+            if (array is null)
+            {
+                throw new ArgumentNullException($"{nameof(array)} can't be null");
+            }
 
-        private static void BubbleSort(int[][] array, IComparer<int[]> comparer)
-        {
+            if (comparer is null)
+            {
+                throw new ArgumentNullException($"{nameof(comparer)} can't be null");
+            }
+
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 var isChanged = false;
@@ -64,16 +71,27 @@
 
         #endregion
 
-        #region Class decorator
+        #region Private methods
+
+        private static void Swap(ref int[] firstRow, ref int[] secondRow)
+        {
+            int[] temp = firstRow;
+            firstRow = secondRow;
+            secondRow = temp;
+        }
+
+        #endregion
+
+        #region Class Adapter
 
         /// <summary>
-        /// Class decorator
+        /// Class adapter
         /// </summary>
-        public class Decorator : IComparer<int[]>
+        public class Adapter : IComparer<int[]>
         {
             private readonly Func<int[], int[], int> _conditionFunc;
 
-            public Decorator(Func<int[], int[], int> conditionFunc)
+            public Adapter(Func<int[], int[], int> conditionFunc)
             {
                 _conditionFunc = conditionFunc;
             }
--- NET.S.2018.Lavrenov.09/JaggedArraySorts/InterfaceMethodJaggedArrayBubbleSort.cs
+++ NET.S.2018.Lavrenov.09/JaggedArraySorts/InterfaceMethodJaggedArrayBubbleSort.cs
@@ -33,19 +33,26 @@
             BubbleSort(array, condition.Compare);
         }
 
-        #endregion
-
-        #region Private methods
-
-        private static void Swap(ref int[] firstRow, ref int[] secondRow)
+        /// <summary>
+        /// Method for sorting jagged arrays
+        /// with given comparision logic
+        /// </summary>
+        /// <param name="array">Array for sorting</param>
+        /// <param name="comparison">Necessary logic for sorting</param>
+        /// <exception cref="ArgumentNullException">if given array is null</exception>
+        /// <exception cref="ArgumentNullException">if given comparison is null</exception>
+        public static void BubbleSort(int[][] array, Comparison<int[]> comparison)
         {
-            int[] temp = firstRow;
-            firstRow = secondRow;
-            secondRow = temp;
-        }
+            if (array is null)
+            {
+                throw new ArgumentNullException($"{nameof(array)} can't be null");
+            }
 
-        private static void BubbleSort(int[][] array, Comparison<int[]> comparison)
-        {
+            if (comparison is null)
+            {
+                throw new ArgumentNullException($"{nameof(comparison)} can't be null");
+            }
+
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 var isChanged = false;
@@ -66,6 +73,17 @@
             }
         }
 
+        #endregion
+
+        #region Private methods
+
+        private static void Swap(ref int[] firstRow, ref int[] secondRow)
+        {
+            int[] temp = firstRow;
+            firstRow = secondRow;
+            secondRow = temp;
+        }
+        
         #endregion
     }
 }
