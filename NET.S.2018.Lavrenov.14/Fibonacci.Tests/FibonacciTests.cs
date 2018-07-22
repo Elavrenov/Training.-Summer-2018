ff2cd8b483973e50c6496791a298bbf990f8ef26
Egor Lavrenov <egorlavenow@gmail.com>
2018-07-22 18:33:10 +0300

    Fibonacci task added

---
+++ NET.S.2018.Lavrenov.14/Fibonacci.Tests/FibonacciTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Fibonacci.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    using FibonacciNumbers;
+    [TestFixture]
+    public class FibonacciTests
+    {
+        [TestCase(10, ExpectedResult = new int[] { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 })]
+        [TestCase(1, ExpectedResult = new int[] { 1 })]
+        [TestCase(5, ExpectedResult = new int[] { 1, 1, 2, 3, 5 })]
+        public int[] FibonacciNumbersTest(int n)
+        {
+            return Fibonacci.GetFibonacci(n);
+        }
+
+        [TestCase(0)]
+        [TestCase(-1)]
+        [TestCase(-5)]
+        public void FibonacciNumbersTestArgumentException(int n)
+        {
+            Assert.Throws<ArgumentException>(() => Fibonacci.GetFibonacci(n));
+        }
+    }
+}
---
+++ NET.S.2018.Lavrenov.14/FibonacciNumbers/Fibonacci.cs
@@ -0,0 +1,34 @@
+﻿namespace FibonacciNumbers
+{
+    using System;
+    public static class Fibonacci
+    {
+        /// <summary>
+        /// Method for searcing Fibonacci numbers
+        /// </summary>
+        /// <param name="amount">Digit of Fibonacci numbers</param>
+        /// <returns>Array with fixed digit Fibonacci numbers</returns>
+        /// <exception cref="ArgumentException">if <param name="amount"> less or equal zero</param></exception>
+        public static int[] GetFibonacci(int amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException($"{amount} can't be less or equal zero");
+            }
+
+            int[] fibArray = new int[amount];
+            fibArray[0] = 1;
+            int a = 0;
+
+            for (int i = 1; i < amount; i++)
+            {
+
+                int temp = fibArray[i - 1] + a;
+                a = fibArray[i - 1];
+                fibArray[i] = temp;
+            }
+
+            return fibArray;
+        }
+    }
+}
