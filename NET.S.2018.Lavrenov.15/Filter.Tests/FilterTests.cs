a5f7cadfef65a0bc7bd17512a513c13a439c3239
Egor Lavrenov <egorlavenow@gmail.com>
2018-07-24 03:44:03 +0300

    Array filter

---
+++ NET.S.2018.Lavrenov.15/Filter.Tests/FilterTests.cs
@@ -0,0 +1,42 @@
+﻿namespace Filter.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class FilterTests
+    {
+        [TestCase(new[] { 1, 2, 3, 4, 0, -1, -1 - 2 }, ExpectedResult = new[] { 1, 2, 3, 4 })]
+        [TestCase(new[] { -1, -2, 3, 4, 0, -1, -1 - 2 }, ExpectedResult = new[] { 3, 4 })]
+        [TestCase(new[] { 1, -12, 3, 4, 0, -1, -1 - 2 }, ExpectedResult = new[] { 1, 3, 4 })]
+        [TestCase(new[] { 1, 22, 3, 4, 0, -1, -1 - 2 }, ExpectedResult = new[] { 1, 22, 3, 4 })]
+        [TestCase(new[] { 1, -11, 4, 0, -1, -1 - 2 }, ExpectedResult = new[] { 1, 4 })]
+
+        public int[] FilterIntTest(int[] array)
+        {
+            return array.Filtration(x => x > 0);
+        }
+
+        [Test]
+        public void FiterStringTest()
+        {
+            string[] actual = new[]
+            {
+                "Va44",
+                "hello",
+                "hell",
+                "h",
+                "razor",
+                "nn"
+            };
+
+            string[] expected = new[]
+            {
+                "hello",
+                "hell",
+                "h",
+            };
+
+            CollectionAssert.AreEqual(expected, actual.Filtration(x => x.Contains("h")));
+        }
+    }
+}
---
+++ NET.S.2018.Lavrenov.15/Filter/Filter.cs
@@ -0,0 +1,56 @@
+﻿namespace Filter
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Filter class
+    /// </summary>
+    public static class Filter
+    {
+        #region Public API
+
+        /// <summary>
+        /// Filter the array depending on the condition
+        /// </summary>
+        /// <typeparam name="T">All types</typeparam>
+        /// <param name="array">T array</param>
+        /// <param name="predicate">condition for filtration</param>
+        /// <returns>Filtred array</returns>
+        public static T[] Filtration<T>(this T[] array, Predicate<T> predicate)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException($"{nameof(array)} can't be null");
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException($"{nameof(predicate)} can't be null");
+            }
+
+            return StartFilter(array, predicate);
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private static T[] StartFilter<T>(IEnumerable<T> enumerable, Predicate<T> predicate)
+        {
+            List<T> filtredList = new List<T>();
+
+            foreach (var item in enumerable)
+            {
+                if (predicate(item))
+                {
+                    filtredList.Add(item);
+                }
+            }
+
+            return filtredList.ToArray();
+        }
+
+        #endregion   
+    }
+}
